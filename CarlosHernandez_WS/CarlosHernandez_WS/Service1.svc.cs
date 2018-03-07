febf904e914b0a454f1b707059a8e1da6ab743e9
CarlosHernandezAvila <36894073+CarlosHernandezAvila@users.noreply.github.com>
2018-03-07 14:05:04 -0400

    Se creo un comentario dentro del Service1.svc.cs

--- CarlosHernandez_WS/CarlosHernandez_WS/Service1.svc.cs
+++ CarlosHernandez_WS/CarlosHernandez_WS/Service1.svc.cs
@@ -8,6 +8,7 @@ using System.Text;
 
 namespace CarlosHernandez_WS
 {
+    // Se estan creado un  nuevo proyecto de conexion a la base de datos SqlServer desde el servicio Web
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
     // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
     public class Service1 : IService1
